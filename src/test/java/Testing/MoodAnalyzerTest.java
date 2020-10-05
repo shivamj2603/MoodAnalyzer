2efe1a2bb37ce7355928d6c62e64f7aee315375c
shivamj2603 <shivamj2603@gmail.com>
2020-10-05 23:34:40 +0530

    Added:Testcase to check for MoodAnalysisException when null

--- src/test/java/Testing/MoodAnalyzerTest.java
+++ src/test/java/Testing/MoodAnalyzerTest.java
@@ -36,6 +36,13 @@ class MoodAnalyzerTest {
 		MoodAnalysisException exception = assertThrows(MoodAnalysisException.class, () -> moodAnalyzer.analyseMood());
 		assertEquals(exception.getType() ,MoodAnalysisException.ExceptionType.Null);
 	}
+	@Test
+	public void givenNull_shouldThrow_MoodAnalysisException() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+		MoodAnalysisException exception = assertThrows(MoodAnalysisException.class, () -> moodAnalyzer.analyseMood());
+		assertEquals(exception.getType() ,MoodAnalysisException.ExceptionType.Null);
+	}
 	
+
 
 }
